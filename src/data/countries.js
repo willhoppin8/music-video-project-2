d187bf058e0859c4de8cf73ec62261ae0ab45f25
williamhoppin <willhoppin8@gmail.com>
2025-02-23 11:58:26 -0500

    add more cuntries

--- src/data/countries.js
+++ src/data/countries.js
@@ -3,7 +3,11 @@ export const countries = [
   { name: "Albania", rotationX: 0.7182605831, rotationY: 0.4179967605, zoom: 0.9 },
   { name: "Algeria", rotationX: 0.4892838572, rotationY: 0.7410345157, zoom: 1 },
   { name: "Angola", rotationX: -0.1955240001, rotationY: 0.4580415948, zoom: 1.2 },
+  { name: "Argentina", rotationX: -0.6704874308, rotationY: 1.880320874, zoom: 1 },
+  { name: "Armenia", rotationX: 0.6993377233, rotationY: -0.01606487917, zoom: 1 },
   { name: "Australia", rotationX: -0.4411214965, rotationY: -1.564815952, zoom: 1.3 },
+  { name: "Austria", rotationX: 0.829314138, rotationY: 0.5160528485, zoom: 1 },
+  { name: "Azerbaijan", rotationX: 0.700629267, rotationY: -0.06037355289, zoom: 1 },
   { name: "Canada", rotationX: 0.9796602905, rotationY: 2.626101809, zoom: 1.5 },
   { name: "Spain", rotationX: 0.7062247925, rotationY: 0.8354358843, zoom: 1 },
 ];
